3a93c8300e7d84d4b0f97a02a3833687753f3564
Vivaan <parashar.xtra@gmail.com>
2021-09-24 21:55:08 +0530

    complete drops; TODO: fill in correct item ids

--- src/entities/mobs.py
+++ src/entities/mobs.py
@@ -4,10 +4,11 @@ from typing import Any, List, Tuple
 import arcade
 
 from src.constants import SPRITE_SCALING
+from src.misc.item import Item
 
 
 class Mob(arcade.Sprite):
-    def __init__(self: Any, health: int, drops: List[Tuple[str, int]]) -> None:
+    def __init__(self: Any, health: int, drops: List[Tuple[int, int]]) -> None:
         self.health = health
         self.drops = drops
 
@@ -15,41 +16,41 @@ class Mob(arcade.Sprite):
         self.change_x += random.choices([0, 3, -3], weights=[3, 1, 1])
 
     def kill(self: Any) -> List:
-        return self.drops
+        return [Item(True, 64, actual_amount=amt, block_id=_id) for amt, _id in self.drops]
 
 
 class Cow(Mob):
-    def __init__(
-        self: Any, x: int, y: int, health: int, drops: List[Tuple[str, int]]
-    ) -> None:
+    def __init__(self: Any, x: int, y: int, health: int) -> None:
 
-        super().__init__(health, drops)
+        super().__init__(
+            health,
+            [(random.randint(1, 4), 300), (random.randint(1, 3), 301)]
+        )
         arcade.Sprite.__init__(
             self, "assets/cow.png", SPRITE_SCALING, center_x=x, center_y=y
         )
 
 
 class Sheep(Mob):
-    def __init__(
-        self: Any, x: int, y: int, health: int, drops: List[Tuple[str, int]]
-    ) -> None:
+    def __init__(self: Any, x: int, y: int, health: int) -> None:
 
-        super().__init__(health, drops)
+        super().__init__(
+            health, [(random.randint(1, 4), 302), (random.randint(1, 3), 303)]
+        )
         arcade.Sprite.__init__(
             self, "assets/sheep.png", SPRITE_SCALING, center_x=x, center_y=y
         )
 
 
 class Chicken(Mob):
-    def __init__(
-        self: Any, x: int, y: int, health: int, drops: List[Tuple[str, int]]
-    ) -> None:
+    def __init__(self: Any, x: int, y: int, health: int) -> None:
 
-        super().__init__(health, drops)
+        super().__init__(
+            health, [(random.randint(1, 4), 303), (random.randint(1, 2), 304)]
+        )
         arcade.Sprite.__init__(
             self, "assets/sheep.png", SPRITE_SCALING, center_x=x, center_y=y
         )
 
 
-# FIXME Right now we are taking in the drops list in __init__, but eventually
-# we can fix the lists, once the item system is sorted.
+# FIXME Fill in correct item (drop) IDs
