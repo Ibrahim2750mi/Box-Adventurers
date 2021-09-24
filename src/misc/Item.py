5207fc573a808b68eb9300f3c6782d2e68b50eca
Vivaan <parashar.xtra@gmail.com>
2021-09-24 21:37:08 +0530

    merge misc folders and fix item.py

--- src/misc/Item.py
+++ src/misc/Item.py
@@ -1,5 +1,9 @@
+import arcade
+
+
 class Item(arcade.Sprite):
-    def __init__(stackable : bool, max_stack: int, inventory_slot: tuple, actual_amount: int, block_id: int, *args, **kwargs):
+    def __init__(self, stackable: bool, max_stack: int, inventory_slot: tuple,
+                 actual_amount: int, block_id: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stackable = stackable
         self.max_stack = max_stack
@@ -7,15 +11,13 @@ class Item(arcade.Sprite):
         self.actaul_amout = actual_amount
         self.block_id = block_id
 
-    def change_inventory_slot(position: tuple) -> None:
+    def change_inventory_slot(self, position: tuple) -> None:
         self.inventory_slot = position
 
-
-    def add_ammount(amount: int) -> None:
+    def add_ammount(self, amount: int) -> None:
         self.actual_amount += amount
 
-    def delete_ammount(amount: int = 0, all: bool = False) -> None:
+    def delete_ammount(self, amount: int = 0, all: bool = False) -> None:
         self.actual_amount -= amount
         if self.actual_amount < 0 or all is True:
             self.kill()
-
