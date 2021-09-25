849f65dd0b8259bb4548d554e282806e0145633d
Vivaan <parashar.xtra@gmail.com>
2021-09-25 22:45:58 +0530

    Add block breaking functionality

--- src/block/block.py
+++ src/block/block.py
@@ -2,31 +2,36 @@ import time
 from pathlib import Path
 
 import arcade
-from PIL import ImageEnhance
+from PIL import Image, ImageEnhance
+
+from src.misc.item import Item
+
+BREAK_TEXTURES_PATH = Path("assets/animations")
+BREAK_TEXTURES = [Image.open(f) for f in BREAK_TEXTURES_PATH.iterdir()]
 
 
-class BreakMask(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.break_state = -1
-        self.textures = []
-        graphics = Path("../../assets/animations")
-        for asset in graphics.iterdir():
-            self.textures.append(arcade.Sprite(asset))
+# class BreakMask(arcade.Sprite):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.break_state = -1
+#         self.textures = []
+#         for asset in graphics.iterdir():
+#             self.textures.append(arcade.Sprite(asset))
 
-    def add_break_state(self):
-        self.break_state += 1
-        self.set_texture(self.textures[self.break_state])
+#     def add_break_state(self):
+#         self.break_state += 1
+#         self.set_texture(self.textures[self.break_state])
 
-    def reset_break_state(self):
-        self.break_state = 0
-        self.set_texture(self.textures[self.break_state])
+#     def reset_break_state(self):
+#         self.break_state = 0
+#         self.set_texture(self.textures[self.break_state])
 
 
 class Block(arcade.Sprite):
     def __init__(self, width, height, breaking_time, hp, block_id, bright, *args, scale=1,
                  center_x=0, center_y=0, **kwargs):
-        path = Path(__file__).parent.joinpath(f"../../assets/Sprites/{block_id}.png")
+        path = Path(__file__).parent.joinpath(
+            f"../../assets/Sprites/{block_id}.png")
         super().__init__(*args, **kwargs,
                          filename=str(path), scale=scale, center_x=center_x, center_y=center_y,
                          image_width=width, image_height=height)
@@ -42,6 +47,15 @@ class Block(arcade.Sprite):
         self.ORIGINAL_IMAGE = self.texture.image
         self.bright = bright
         self.breaking_time = breaking_time
+        self.break_time_left = breaking_time
+        self.break_textures = []
+        for i, break_texture in BREAK_TEXTURES:
+            orig_textur_copy = self._texture.image.copy()
+            orig_textur_copy.paste(break_texture, )
+            new_texture = arcade.Texture(f"{block_id}_{i}", orig_textur_copy)
+            self.break_textures.append(new_texture)
+        self.anim_pos = 0
+        self.orig_texture = self._texture
 
     def hp_set(self, val):
         if val <= 0:
@@ -61,3 +75,16 @@ class Block(arcade.Sprite):
         self.bright = bright
         enhancer = ImageEnhance.Brightness(self.ORIGINAL_IMAGE)
         self.texture.image = enhancer.enhance(bright)
+
+    def _break(self, delta_time) -> None:
+        self.break_time_left -= delta_time
+        if self.breaking_time < 0:
+            return Item(stackable=True, max_stack=64, inventory_slot=0, actual_amount=1, block_id=self.block_id)
+        else:
+            self.anim_pos += 1
+            self.texture = self.break_textures[self.anim_pos]
+
+    def stop_breaking(self) -> None:
+        # Reset time and texture
+        self.break_time_left = self.breaking_time
+        self.texture = self.orig_texture
